d7881a5874bf890ba27bf1349350c8ce3892b4a2
Paunlol <jiraphat4352@gmail.com>
2023-12-01 09:26:58 +0700

    Index

---
+++ js/Script.js
@@ -0,0 +1,10 @@
+let Eaten = 1500;
+let Total = 2500;
+
+function getValues() {
+    return {
+        Eaten: Eaten,
+        Total: Total
+    };
+}
+export { getValues };
--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -1,17 +1,17 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
-
+// console.log("asdasd", Eaten);
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["getKcal", "noKcal",],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [50, 50],
+      backgroundColor: ['#FF8A00', '#e8e8e8'],
+      hoverBackgroundColor: ['#ffae51', '#e8e8e8'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
